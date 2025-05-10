1b5537c1d713928b06627bf813b3aba5fe570769
Cristian Gutierrez <cdgutierrez456@gmail.com>
2025-05-10 02:08:40 -0500

    Agregando ruta y controlador de tasks

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,16 @@
+import { pool } from "../libs/db.js";
+
+import { errorHandler } from "../helpers/errorHandler.js";
+
+export const getAll = (req, res) => {
+  pool.query('select * from task')
+  .then((data) => {
+    const info = data[0]
+    res.json({
+      data: info
+    })
+  })
+  .catch(error => {
+    errorHandler(res, 404, "Error al obtener la informacion de las rutas", error)
+  })
+}
